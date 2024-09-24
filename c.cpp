42c4ca9d6a4ebe4ec46179a0c40780ee38bfa033
Afsana Akter Mim <162867405+afsanamim04@users.noreply.github.com>
2024-09-24 10:56:02 +0600

    Robin Hood and Town

---
+++ c.cpp
@@ -0,0 +1,30 @@
+// In The Name of Allah
+#include <bits/stdc++.h>
+#define PI acos(-1.0)
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define pb push_back
+typedef long long int ll;
+typedef unsigned long long int llu;
+using namespace std;
+
+void sol()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
+}
+
+
+
